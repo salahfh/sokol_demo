ab576c99f1a3d1388143341117e407724f5ae6ba
Saoud Salah Eddine <salah_fh@outlook.com>
2026-09-20 02:10:35 -0500

    Basic shaders setup.

--- Makefile
+++ Makefile
@@ -4,21 +4,29 @@
 CC=gcc
 CFLAGS+=-std=gnu23
 CFLAGS+=-Wall -Wextra -Wshadow -Wno-unused-function -Werror -Wno-missing-field-initializers
-INCS+=-Ideps
+INCS+=-Ideps -Ishaders
 DEFS+=-DSOKOL_GLCORE
 LIBS+=-lX11 -lXi -lXcursor -lGL -ldl -lm
 LSP+=$(DEFS) $(INCS)
 OUTDIR=build
 OUTEXC=gui
 
+SHDC=sokol-shdc
+# SHDCFLAGS=--format sokol_impl --slang glsl410
+SHDCFLAGS=--format sokol_impl --slang glsl430:hlsl5:metal_macos
+SHADERS= \
+	 shaders/shd.glsl.h
+
 all: $(OUTEXC)
+
+shaders: $(SHADERS)
 
 
 $(OUTDIR)/deps.o: deps/deps.c
 	@mkdir -p $(OUTDIR)
 	$(CC) -c $(CFLAGS) $(DEFS) -o $@ deps/deps.c
 
-$(OUTEXC): $(OUTDIR)/deps.o src/main.c
+$(OUTEXC): $(OUTDIR)/deps.o shaders/*.h src/main.c
 	$(CC) $(CFLAGS) $(INCS) $(LIBS) $(OUTDIR)/deps.o -o $(OUTDIR)/$(OUTEXC) src/main.c
 
 run: $(OUTEXC)
@@ -34,7 +42,15 @@ compile_flags.txt: FORCE
 
 FORCE:
 
-sokol_update:
+shaders/%.glsl.h: shaders/%.glsl
+	$(SHDC) $(SHDCFLAGS) -i $^ -o $@
+
+clean-shaders:
+	rm -f $(SHADERS)
+
+update-thirdparty:
 	wget -O deps/sokol_gfx.h https://raw.githubusercontent.com/floooh/sokol/refs/heads/master/sokol_gfx.h
 	wget -O deps/sokol_app.h https://raw.githubusercontent.com/floooh/sokol/refs/heads/master/sokol_app.h
 	wget -O deps/sokol_glue.h https://raw.githubusercontent.com/floooh/sokol/refs/heads/master/sokol_glue.h
+
+.PHONY: all shaders clean clean-shaders update-thirdparty
--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 #include "stdio.h"
 
+// sokol
 #include "sokol_app.h"
 #include "sokol_gfx.h"
 #include "sokol_glue.h"
+
+// shaders
+#define SOKOL_SHDC_IMPL
+#include "shd.glsl.h"
 
 static struct {
   sg_pipeline pip;
@@ -15,16 +20,43 @@ void init(void) {
       .environment = sglue_environment(),
   });
 
+  float vertices[] = {
+      // position         //  colors
+      0.0f,  0.5f,  0.5f, //  1.0f, 0.0f, 0.0f, 1.0f,
+      0.5f,  -0.5f, 0.5f, //  0.0f, 1.0f, 0.0f, 1.0f,
+      -0.5f, -0.5f, 0.5f, //  0.0f, 0.0f, 1.0f, 1.0f};
+  };
+
+  state.bind.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
+      .data = SG_RANGE(vertices),
+      .label = "triangle_verties",
+  });
+
+  sg_shader shd = sg_make_shader(shd_shader_desc(sg_query_backend()));
+
+  state.pip = sg_make_pipeline(&(sg_pipeline_desc){
+      .shader = shd,
+      .layout =
+          {
+              .attrs = {[ATTR_shd_pos].format = SG_VERTEXFORMAT_FLOAT3},
+
+          },
+      .label = "shader"});
+
   state.pass_action =
       (sg_pass_action){.colors[0] = {.load_action = SG_LOADACTION_CLEAR,
                                      .clear_value = {.2f, .2f, .2f, 1.0f}}};
-};
+}
 
 void frame() {
   sg_begin_pass(&(sg_pass){
       .action = state.pass_action,
       .swapchain = sglue_swapchain(),
   });
+
+  sg_apply_pipeline(state.pip);
+  sg_apply_bindings(&state.bind);
+  sg_draw(0, 3, 1);
 
   sg_end_pass();
   sg_commit();
@@ -42,7 +74,6 @@ void event(const sapp_event *ev) {
 void cleanup() { sg_shutdown(); }
 
 sapp_desc sokol_main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
-  printf("Hello world \n");
 
   return (sapp_desc){
       .width = 640,
